97094f8914ccb84b4519947be323a9676eba3c57
jeffyactive <jeff@reelyactive.com>
2023-03-08 11:20:29 -0500

    Updated to cormorant@2.0.2

--- js/cormorant.js
+++ js/cormorant.js
@@ -22,44 +22,41 @@ let cormorant = (function() {
     }
     let startIndex = html.indexOf('>', tagIndex) + 1;
     let stopIndex = html.indexOf('</script>', startIndex);
-    let jsonString = html.substring(startIndex, stopIndex);
-    
-    return parseAsStory(jsonString);
-  }
-
-  // Parse the given stringified JSON as a standardised story
-  function parseAsStory(jsonString) {
-    let json = null;
 
     try {
-      json = JSON.parse(jsonString);
-
-      // Handle standard reelyActive API response case
-      if(json.hasOwnProperty('stories')) {
-        let storyId = Object.keys(json.stories)[0];
-        let story = json.stories[storyId];
-        return story;
-      }
+      return parseAsStory(JSON.parse(html.substring(startIndex, stopIndex)));
     }
-    catch(e) { }
+    catch(error) {
+      return null;
+    }
+  }
 
-    return json;
+  // Parse the given JSON as a standardised story
+  function parseAsStory(data) {
+    // Handle standard reelyActive API response case
+    if(data.hasOwnProperty('stories')) {
+      let storyId = Object.keys(data.stories)[0];
+      let story = data.stories[storyId];
+
+      return story;
+    }
+
+    return data;
   }
 
   // Perform a HTTP GET on the given URL with the given accept headers
   function retrieve(url, acceptHeaders, callback) {
-    let httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange = () => {
-      if(httpRequest.readyState === XMLHttpRequest.DONE) {
-        let contentType = httpRequest.getResponseHeader('Content-Type');
-        return callback(httpRequest.status, httpRequest.responseText,
-                        contentType);
-      }
-    };
-    httpRequest.open('GET', url);
-    httpRequest.setRequestHeader('Accept', acceptHeaders);
-    httpRequest.send();
+    fetch(url, { headers: { "Accept": acceptHeaders } })
+      .then((response) => {
+        if(!response.ok) { throw new Error('GET returned ' + response.status); }
+        let contentType = response.headers.get('Content-Type');
+        if(contentType.startsWith('application/json')) {
+          return response.json();
+        }
+        return response.text();
+      })
+      .then((result) => { return callback(result); })
+      .catch((error) => { return callback(null); });
   }
 
   // Get the associations for the given device signature
@@ -67,28 +64,29 @@ let cormorant = (function() {
     options = options || {};
     let url = serverUrl + '/associations/' + deviceSignature;
 
-    retrieve(url, 'application/json', (status, responseText) => {
+    retrieve(url, 'application/json', (data) => {
       let deviceAssociations = null;
       let isStoryBeingRetrieved = false;
+      let isJsonData = (typeof data === 'object');
 
-      if(status === STATUS_OK) {
-        let response = JSON.parse(responseText);
+      if(isJsonData) {
         let returnedDeviceId = null;
-        if(response.hasOwnProperty('associations')) { // chickadee v1.x
-          returnedDeviceSignature = Object.keys(response.associations)[0];
-          deviceAssociations = response.associations[returnedDeviceSignature];
+        if(data.hasOwnProperty('associations')) { // chickadee v1.x
+          returnedDeviceSignature = Object.keys(data.associations)[0];
+          deviceAssociations = data.associations[returnedDeviceSignature];
         }
-        else if(response.hasOwnProperty('devices')) { // chickadee v0.x
-          returnedDeviceSignature = Object.keys(response.devices)[0];
-          deviceAssociations = response.devices[returnedDeviceSignature];
+        else if(data.hasOwnProperty('devices')) { // chickadee v0.x
+          returnedDeviceSignature = Object.keys(data.devices)[0];
+          deviceAssociations = data.devices[returnedDeviceSignature];
         }
         associations.set(deviceSignature, deviceAssociations);
         associations.set(returnedDeviceSignature, deviceAssociations);
 
         if(options.isStoryToBeRetrieved && deviceAssociations.url) {
           isStoryBeingRetrieved = true;
-          retrieveStory(deviceAssociations.url, options, (story, status) => {
-            return callback(deviceAssociations, story, status);
+          retrieveStory(deviceAssociations.url, options,
+                        (story, isRetrievedFromMemory) => {
+            return callback(deviceAssociations, story, isRetrievedFromMemory);
           });
         }
       }
@@ -104,27 +102,17 @@ let cormorant = (function() {
     options = options || {};
 
     if(stories.has(storyUrl) && !options.isStoryToBeRefetched) {
-      return callback(stories.get(storyUrl), undefined);
+      return callback(stories.get(storyUrl), true);
     }
 
-    retrieve(storyUrl, 'application/json, text/plain',
-             (status, responseText, contentType) => {
-      if(status !== STATUS_OK) {
-        return callback(null, status);
-      }
+    retrieve(storyUrl, 'application/json, text/plain', (data) => {
+      if(!data) { return callback(null, false); }
 
-      let isJson = (contentType.indexOf('application/json') === 0);
-      let story;
-      if(isJson) {
-        story = parseAsStory(responseText);
-      }
-      else {
-        story = extractFromHtml(responseText);
-      }
-      if(story) {
-        stories.set(storyUrl, story);
-      }
-      return callback(story, status);
+      let isJsonData = (typeof data === 'object');
+      story = isJsonData ? parseAsStory(data) : extractFromHtml(data);
+      if(story) { stories.set(storyUrl, story); }
+
+      return callback(story, false);
     });
   }
 
