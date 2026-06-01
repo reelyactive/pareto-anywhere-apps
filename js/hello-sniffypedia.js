a4ce6c76c24b8793b1417e913e93ba4c56f78ece
jeffyactive <jeff@reelyactive.com>
2026-06-01 17:13:16 -0400

    Refreshed Hello Sniffypedia

--- js/hello-sniffypedia.js
+++ js/hello-sniffypedia.js
@@ -1,13 +1,10 @@
 /**
- * Copyright reelyActive 2021-2023
+ * Copyright reelyActive 2021-2026
  * We believe in an open Internet of Things
  */
 
 
 // Constants
-const STATUS_OK = 200;
-const SIGNATURE_SEPARATOR = '/';
-const DIRECTORY_SEPARATOR = ':';
 const POLLING_INTERVAL_MILLISECONDS = 5000;
 const CONTEXT_ROUTE = '/context';
 const SNIFFYPEDIA_BASE_URI = 'https://sniffypedia.org/';
@@ -16,15 +13,12 @@ const DEMO_SEARCH_PARAMETER = 'demo';
 // DOM elements
 let connectIcon = document.querySelector('#connectIcon');
 let demoalert = document.querySelector('#demoalert');
-let firsthalf = document.querySelector('#firsthalf');
-let secondhalf = document.querySelector('#secondhalf');
+let cards = document.querySelector('#cards');
 
 // Other variables
 let baseUrl = window.location.protocol + '//' + window.location.hostname +
               ':' + window.location.port;
-let selectedUrl = baseUrl + CONTEXT_ROUTE;
 let isPollPending = false;
-let pollingInterval;
 
 // Initialise based on URL search parameters, if any
 let searchParams = new URLSearchParams(location.search);
@@ -36,85 +30,56 @@ if(isDemo) {
   connectIcon.replaceChildren(demoIcon);
 }
 
+// Handle beaver events
+beaver.on('connect', () => {
+  connectIcon.replaceChildren(createElement('i', 'fas fa-cloud text-success'));
+  demoalert.hidden = true;
+});
+beaver.on('poll', (isSuccess) => {
+  connectIcon.replaceChildren(createElement('i', 'fas fa-cloud text-success'));
+  demoalert.hidden = isSuccess;
+  updateDisplay(beaver.devices);
+  isPollPending = false;
+});
+beaver.on('error', () => {
+  connectIcon.replaceChildren(createElement('i', 'fas fa-cloud text-danger'));
+  demoalert.hidden = false;
+});
 
 setInterval(pollAndDisplay, POLLING_INTERVAL_MILLISECONDS);
 pollAndDisplay();
 
 
-// GET the devices and display in DOM
+// Poll the devices and display in DOM
 function pollAndDisplay() {
   if(!isPollPending) {
     isPollPending = true;
 
     if(isDemo) {
-      let response = starling.getContext();
-      let devices = response.devices || {};
-      isPollPending = false;
+      let response = starling.getContext(CONTEXT_ROUTE);
+      devices = new Map(Object.entries(response.devices || {}));
       updateDisplay(devices);
-      return;
-    }
-
-    getContext(selectedUrl, function(status, response) {
-      let statusIcon = createElement('i', 'fas fa-cloud text-danger');
       isPollPending = false;
-
-      if(status === STATUS_OK) {
-        let devices = JSON.parse(response).devices || {};
-        statusIcon = createElement('i', 'fas fa-cloud text-success');
-        demoalert.hidden = true;
-        updateDisplay(devices);
-      }
-      else {
-        connectIcon.hidden = false;
-        demoalert.hidden = false;
-        updateDisplay({});
-      }
-
-      connectIcon.replaceChildren(statusIcon);
-    });
-  }
-}
-
-
-// GET the context
-function getContext(url, callback) {
-  let httpRequest = new XMLHttpRequest();
-
-  httpRequest.onreadystatechange = function() {
-    if(httpRequest.readyState === XMLHttpRequest.DONE) {
-      return callback(httpRequest.status, httpRequest.responseText);
     }
-  };
-  httpRequest.open('GET', url);
-  httpRequest.setRequestHeader('Accept', 'application/json');
-  httpRequest.send();
+    else {
+      beaver.poll(baseUrl, { clearDevices: true });
+    }
+  }
 }
 
 
 // Update the display based on the latest devices
 function updateDisplay(devices) {
   let sniffypediaUris = tallyStatid(devices);
-  let firstGroup = new DocumentFragment();
-  let secondGroup = new DocumentFragment();
+  cards.replaceChildren();
 
-  if(sniffypediaUris.size > 0) {
-    let halfwayMark = Math.ceil(sniffypediaUris.size / 2);
-    let maxCount = sniffypediaUris.entries().next().value[1];
-    let offset = 0;
-
-    fetchStories(sniffypediaUris.keys());
-    sniffypediaUris.forEach((count, uri) => {
-      if(offset++ < halfwayMark) {
-        appendCard(firstGroup, uri, count, maxCount);
-      }
-      else {
-        appendCard(secondGroup, uri, count, maxCount);
-      }
+  sniffypediaUris.forEach((count, uri) => {
+    cormorant.retrieveStory(uri, { isStoryToBeRefetched: false },
+                            (story, status) => {
+      let card = createCard(uri, count);
+      cards.appendChild(card);
     });
-  }
-
-  firsthalf.replaceChildren(firstGroup);
-  secondhalf.replaceChildren(secondGroup);
+  });
 }
 
 
@@ -122,9 +87,7 @@ function updateDisplay(devices) {
 function tallyStatid(devices) {
   let tallies = new Map();
 
-  for(const deviceSignature in devices) {
-    let device = devices[deviceSignature];
-
+  devices.forEach((device) => {
     if(device.statid) {
       let hasSniffypedia = device.statid.uri &&
                            device.statid.uri.startsWith(SNIFFYPEDIA_BASE_URI);
@@ -133,47 +96,32 @@ function tallyStatid(devices) {
         tallies.set(device.statid.uri, count + 1);
       }
     }
-  }
+  });
 
   return new Map([...tallies.entries()].sort((a, b) => b[1] - a[1]));
 }
 
 
-// Create and append a card with device count
-function appendCard(parent, uri, count, maxCount) {
+// Create a card with image, name and device count
+function createCard(uri, count) {
   let img = '';
-  let title = uri.substring(SNIFFYPEDIA_BASE_URI.length);
+  let name = uri.substring(SNIFFYPEDIA_BASE_URI.length);
   let story = cormorant.stories.get(uri);
 
   if(story) {
-    img = createElement('img', 'img-fluid rounded-start');
+    img = createElement('img', 'card-img-top');
     img.setAttribute('src', cuttlefishStory.determineImageUrl(story));
-    title = cuttlefishStory.determineTitle(story);
+    name = cuttlefishStory.determineTitle(story) || '-';
   }
 
-  let cardTitle = createElement('h4', 'card-title', title);
-  let progressBar = createElement('div', 'progress-bar', count);
-  let progress = createElement('div', 'progress', progressBar);
-  let cardText = createElement('p', 'card-text', progress);
-  let cardBody = createElement('div', 'card-body', [ cardTitle, cardText ]);
-  let rightCol = createElement('div', 'col-9 col-sm-10', cardBody);
-  let leftCol = createElement('div', 'col-3 col-sm-2', img);
-  let row = createElement('div', 'row g-0', [ leftCol, rightCol ]);
-  let card = createElement('div', 'card hover-shadow mb-3', row);
-  let widthPercentage = Math.floor(100 * count / maxCount);
+  let cardTitle = createElement('p', 'card-title text-center display-4', count);
+  let footerText = createElement('small', 'text-truncate', name);
+  let cardFooter = createElement('div', 'card-footer text-center', footerText);
+  let cardBody = createElement('div', 'card-body', cardTitle);
+  let card = createElement('div', 'card hover-shadow mb-3',
+                           [ img, cardBody, cardFooter ]);
 
-  progressBar.setAttribute('style', 'width: ' + widthPercentage + '%');
-
-  parent.appendChild(card);
-}
-
-
-// Fetch stories from devices with URIs
-function fetchStories(uris) {
-  for(const uri of uris) {
-    cormorant.retrieveStory(uri, { isStoryToBeRefetched: false },
-                            (story, status) => {});
-  }
+  return createElement('div', 'col', card);
 }
 
 
